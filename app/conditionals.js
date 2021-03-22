2dcc63355e4e6d200f376dc99d03db93780ad0ad
ethanboone <eboone2019@gmail.com>
2021-03-22 14:35:49 -0600

    solved conditional test

--- app/conditionals.js
+++ app/conditionals.js
@@ -1,7 +1,11 @@
 //1. Given two numbers, write a function that will return  the larger number
 
 function largerNum(num1, num2) {
-    // you code here
+    if (num1 > num2) {
+        return num1
+    } else if (num2 > num1) {
+        return num2
+    }
 
 }
 
